c486941a3eaf47d62eadb244e0b463f6ffdf1a0e
gooseish <cegooseish@gmail.com>
2025-12-21 09:55:48 -0500

    still need to figure out how to swap the colors of the exp/skill bars

--- Tactile/Source Code/Sprites/Windowskins/Status_Top_Panel_Bg.cs
+++ Tactile/Source Code/Sprites/Windowskins/Status_Top_Panel_Bg.cs
@@ -44,10 +44,10 @@ namespace Tactile
                     sprite_batch.Draw(textures[0], loc - draw_offset + new Vector2(96 + 192, 76), new Rectangle(0, 107, 29, 7), tint);
                     // Battle Stats BG
                     sprite_batch.Draw(textures[0], loc - draw_offset + new Vector2(196, 8),
-                        new Rectangle(96, 0 + 64 * 2, 120, 64), tint);
+                        new Rectangle(96, 0 + 64 * team_color, 120, 64), tint);
                     // Name Banner
                     sprite_batch.Draw(textures[0], loc - draw_offset + new Vector2(100, 0),
-                        new Rectangle(0, 114 + 25 * 2, 93, 25), tint);
+                        new Rectangle(0, 114 + 25 * team_color, 93, 25), tint);
                 }
         }
     }
